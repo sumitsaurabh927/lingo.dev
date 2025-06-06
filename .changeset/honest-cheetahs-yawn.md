1f9db11a53d8c75ce0e83517b73d43544d0f0fd2
maxprilutskiy <maks.prilutskiy@gmail.com>
2025-06-06 16:27:43 -0700

    chore: lingoprovidewrapper

---
+++ .changeset/honest-cheetahs-yawn.md
@@ -0,0 +1,5 @@
+---
+"@lingo.dev/_react": patch
+---
+
+add console log to lingoproviderwrapper
--- packages/react/src/client/provider.tsx
+++ packages/react/src/client/provider.tsx
@@ -34,9 +34,17 @@ export function LingoProviderWrapper<D>(props: LingoProviderWrapperProps<D>) {
   // for client-side rendered apps, the dictionary is also loaded on the client
   useEffect(() => {
     (async () => {
-      const locale = getLocaleFromCookies();
-      const localeDictionary = await props.loadDictionary(locale);
-      setDictionary(localeDictionary);
+      try {
+        const locale = getLocaleFromCookies();
+        const localeDictionary = await props.loadDictionary(locale);
+        console.log({
+          locale,
+          localeDictionary,
+        });
+        setDictionary(localeDictionary);
+      } catch (error) {
+        console.error("Failed to load dictionary:", error);
+      }
     })();
   }, []);
 
