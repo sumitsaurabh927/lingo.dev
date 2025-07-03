83db104d1ddc7d2fe6b9f14992fc91f3de557ee8
David Turnbull <4712958+davidturnbull@users.noreply.github.com>
2025-07-03 21:46:07 +1000

    docs: add reference docs for CompilerParams (#987)

--- packages/compiler/src/_base.ts
+++ packages/compiler/src/_base.ts
@@ -1,16 +1,83 @@
 import generate, { GeneratorResult } from "@babel/generator";
 import * as t from "@babel/types";
 import * as parser from "@babel/parser";
-import * as path from "path";
 
+/**
+ * Options for configuring Lingo.dev Compiler.
+ */
 export type CompilerParams = {
+  /**
+   * The locale to translate from.
+   *
+   * This must match one of the following formats:
+   *
+   * - [ISO 639-1 language code](https://en.wikipedia.org/wiki/ISO_639-1) (e.g., `"en"`)
+   * - [IETF BCP 47 language tag](https://en.wikipedia.org/wiki/IETF_language_tag) (e.g., `"en-US"`)
+   *
+   * @default "en"
+   */
   sourceLocale: string;
+  /**
+   * The locale(s) to translate to.
+   *
+   * Each locale must match one of the following formats:
+   *
+   * - [ISO 639-1 language code](https://en.wikipedia.org/wiki/ISO_639-1) (e.g., `"en"`)
+   * - [IETF BCP 47 language tag](https://en.wikipedia.org/wiki/IETF_language_tag) (e.g., `"en-US"`)
+   *
+   * @default ["es"]
+   */
   targetLocales: string[];
+  /**
+   * The name of the directory where translation files will be stored, relative to `sourceRoot`.
+   *
+   * @default "lingo"
+   */
   lingoDir: string;
+  /**
+   * The directory of the source code that will be translated, relative to the current working directory.
+   *
+   * @default "src"
+   */
   sourceRoot: string;
+  /**
+   * If `true`, the compiler will generate code for React Server Components (RSC).
+   *
+   * When using Vite, this value is always `false`.
+   *
+   * When using Next.js, this value is always `true`.
+   *
+   * @default false
+   */
   rsc: boolean;
+  /**
+   * If `true`, the compiler will only localize files that use the `"use i18n";` directive.
+   *
+   * @default false
+   */
   useDirective: boolean;
+  /**
+   * If `true`, the compiler will log additional information to the console.
+   *
+   * @default false
+   */
   debug: boolean;
+  /**
+   * The model(s) to use for translation.
+   *
+   * If set to `"lingo.dev"`, the compiler will use Lingo.dev Engine.
+   *
+   * If set to an object, the compiler will use the model(s) specified in the object:
+   *
+   * - The key is a string that represents the source and target locales, separated by a colon (e.g., `"en:es"`).
+   * - The value is a string that represents the LLM provider and model, separated by a colon (e.g., `"google:gemini-2.0-flash"`).
+   *
+   * You can use `*` as a wildcard to match any locale.
+   *
+   * If a model is not specified, an error will be thrown.
+   *
+   * @default {}
+   */
   models: "lingo.dev" | Record<string, string>;
 };
 export type CompilerInput = {
