d38e70f309602dbe0f8cf74074c0da3b98441b53
David Turnbull <realdavidturnbull@gmail.com>
2025-07-24 13:50:34 +1000

    feat: add compiler

---
+++ demo/vite-react-ts/src/lingo/dictionary.js
@@ -0,0 +1,51 @@
+export default {
+  version: 0.1,
+  files: {
+    "App.tsx": {
+      entries: {
+        "2/body/0/argument/1": {
+          content: {
+            en: "Hello, world!",
+            es: "¡Hola, mundo!",
+          },
+          hash: "0468579ef2fbc83c9d520c2f2f1c5059",
+        },
+        "2/body/0/argument/1/1": {
+          content: {
+            en: "Hello, world!",
+            es: "¡Hola, mundo!",
+          },
+          hash: "0468579ef2fbc83c9d520c2f2f1c5059",
+        },
+        "2/body/0/argument/1/3": {
+          content: {
+            en: "This is an example app that demonstrates how <element:strong>Lingo.dev Compiler</element:strong> can be used to localize apps built with <element:a>create-t3-app</element:a> .",
+            es: "Esta es una aplicación de ejemplo que demuestra cómo el <element:strong>Compilador de Lingo.dev</element:strong> puede ser utilizado para localizar aplicaciones construidas con <element:a>create-t3-app</element:a>.",
+          },
+          hash: "de4fc4c1eb45786c32d3cd1b6917f1ec",
+        },
+        "2/body/0/argument/1/5": {
+          content: {
+            en: "To switch between locales, use the following dropdown:",
+            es: "Para cambiar entre idiomas, utiliza el siguiente menú desplegable:",
+          },
+          hash: "1b1e20634dca3d8822e07be1d7bfc144",
+        },
+        "2/body/0/argument/3": {
+          content: {
+            en: "This is an example app that demonstrates how <element:strong>Lingo.dev Compiler</element:strong> can be used to localize apps built with <element:a>Vite</element:a>.",
+            es: "Esta es una aplicación de ejemplo que demuestra cómo <element:strong>Lingo.dev Compiler</element:strong> puede ser utilizado para localizar aplicaciones construidas con <element:a>Vite</element:a>.",
+          },
+          hash: "09ce20a2cb972ab159a82442ad7425fb",
+        },
+        "2/body/0/argument/5": {
+          content: {
+            en: "To switch between locales, use the following dropdown:",
+            es: "Para cambiar entre idiomas, utiliza el siguiente menú desplegable:",
+          },
+          hash: "8e00af4856a0f065107d847737bee094",
+        },
+      },
+    },
+  },
+};
--- demo/vite-react-ts/vite.config.ts
+++ demo/vite-react-ts/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import lingoCompiler from "lingo.dev/compiler";
+import { defineConfig, type UserConfig } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
+const viteConfig: UserConfig = {
   plugins: [react()],
-})
+};
+
+export default defineConfig(() =>
+  lingoCompiler.vite({
+    sourceRoot: "src",
+    lingoDir: "lingo",
+    sourceLocale: "en",
+    targetLocales: ["es"],
+    rsc: false,
+    useDirective: false,
+    debug: false,
+    models: "lingo.dev",
+  })(viteConfig),
+);
