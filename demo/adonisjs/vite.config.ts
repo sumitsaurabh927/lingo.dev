47b2e15d3bba5a64415f851e92215a58f6bc5bba
David Turnbull <realdavidturnbull@gmail.com>
2025-07-27 19:16:07 +0200

    chore: refactor

--- demo/adonisjs/vite.config.ts
+++ demo/adonisjs/vite.config.ts
@@ -1,21 +1,24 @@
-import { defineConfig, type UserConfig } from 'vite'
 import { getDirname } from '@adonisjs/core/helpers'
 import inertia from '@adonisjs/inertia/client'
-import react from '@vitejs/plugin-react'
 import adonisjs from '@adonisjs/vite/client'
+import react from '@vitejs/plugin-react'
 import lingoCompiler from 'lingo.dev/compiler'
+import { type UserConfig } from 'vite'
 
 const viteConfig: UserConfig = {
   plugins: [
-    inertia({ ssr: { enabled: true, entrypoint: 'inertia/app/ssr.tsx' } }),
+    inertia({
+      ssr: {
+        enabled: true,
+        entrypoint: 'inertia/app/ssr.tsx',
+      },
+    }),
     react(),
-    adonisjs({ entrypoints: ['inertia/app/app.tsx'], reload: ['resources/views/**/*.edge'] }),
+    adonisjs({
+      entrypoints: ['inertia/app/app.tsx'],
+      reload: ['resources/views/**/*.edge'],
+    }),
   ],
-
-  /**
-   * Define aliases for importing modules from
-   * your frontend code
-   */
   resolve: {
     alias: {
       '~/': `${getDirname(import.meta.url)}/inertia/`,
@@ -23,15 +26,15 @@ const viteConfig: UserConfig = {
   },
 }
 
-export default defineConfig(() =>
-  lingoCompiler.vite({
-    sourceRoot: 'inertia',
-    lingoDir: 'lingo',
-    sourceLocale: 'en',
-    targetLocales: ['es'],
-    rsc: false,
-    useDirective: false,
-    debug: true,
-    models: 'lingo.dev',
-  })(viteConfig)
-)
+const withLingo = lingoCompiler.vite({
+  sourceRoot: 'inertia',
+  lingoDir: 'lingo',
+  sourceLocale: 'en',
+  targetLocales: ['es'],
+  rsc: false,
+  useDirective: false,
+  debug: false,
+  models: 'lingo.dev',
+})
+
+export default withLingo(viteConfig)
